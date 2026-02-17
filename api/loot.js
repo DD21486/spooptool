41a5d8bc9d1dc1b503baf5fed7347d0841162c7b
DD21486 <daleyvisuals@gmail.com>
2026-02-16 20:31:45 -0500

    Loot 5000

--- api/loot.js
+++ api/loot.js
@@ -60,7 +60,7 @@ function send500(res, detail) {
   return res.status(500).json({ error: 'Server error', detail: detail || 'Unknown error' });
 }
 
-const DISCORD_BIG_DROP_THRESHOLD_GP = 200_000; // temporary; was 400k
+const DISCORD_BIG_DROP_THRESHOLD_GP = 5_000; // temporary; was 400k
 
 /**
  * POST to Discord incoming webhook with an embed for a big loot drop.
