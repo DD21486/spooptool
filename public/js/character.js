6de65aebb6df84f66c93a67d371ce40e005aed45
DD21486 <daleyvisuals@gmail.com>
2026-03-16 08:45:07 -0400

    Brutus 0.3

--- public/js/character.js
+++ public/js/character.js
@@ -337,7 +337,7 @@
 
   /** Boss name (normalized) -> points per kill. API may return "The Whisperer" or "Whisperer"; we map both forms where applicable. */
   const BOSS_POINTS = {
-    brutus: 0.5,
+  brutus: 0.3,
     wintertodt: 1, kraken: 1,
     tempoross: 2, bryophyta: 2, giant_mole: 2, giantmole: 2, hespori: 2, obor: 2, scurrius: 2, shellbane_gryphon: 2, shellbanegryphon: 2,
     amoxliatl: 3, barrows: 3, crazy_archaeologist: 3, crazyarchaeologist: 3, deranged_archaeologist: 3, derangedarchaeologist: 3, grotesque_guardians: 3, grotesqueguardians: 3, king_black_dragon: 3, kingblackdragon: 3, lunar_chests: 3, lunarchests: 3, tombs_of_amascut_entry_mode: 3, the_hueycoatl: 3, hueycoatl: 3, the_royal_titans: 3, royal_titans: 3, royaltitans: 3,
